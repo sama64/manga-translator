df88b067b51233ce9244a71fdf3f8a6fe7ef7c91
sam64 <santiamaya88@gmail.com>
2023-10-15 23:18:45 -0300

    train script fixes

--- scripts/train.py
+++ scripts/train.py
@@ -3,11 +3,16 @@ from ultralytics import YOLO
 
 root_folder_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
+save_dir = f"{root_folder_path}/models"
+os.makedirs(save_dir, exist_ok=True)
+
 # detection
 model = YOLO(f"{root_folder_path}/models/detection.pt")  # YOLO("yolov8m-seg.pt")
 
 model.train(
-    data=f"{root_folder_path}/train_configs/train_detect.yaml", epochs=1000, batch=-1, device=0, workers=0, patience=100
+    data=f"{root_folder_path}/train_configs/train_detect.yaml", 
+    epochs=1, batch=-1, device=0, workers=0, patience=100,
+    project=save_dir
 )
 
 # #segmentation
